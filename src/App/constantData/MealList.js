4de41e5dbdfebad6ed4d32241eff96380e63dcdb
Asad Mumtaz <asadmumtaz92@gmail.com>
2023-07-07 09:15:58 +0500

    This commit for add new meal that is Pizza...

--- src/App/constantData/MealList.js
+++ src/App/constantData/MealList.js
@@ -1,26 +1,39 @@
+import burger from '../assets/images/foodItemIcon.png'
+import pizza from '../assets/images/pizza.png'
 export const DUMMY_MEALS = [
     {
         id: 'm1',
         name: 'Sushi',
         description: 'Finest fish and veggies',
         price: 22.99,
+        image: 'https://static.wixstatic.com/media/d92b55_fb6f2e9db9f648a481576c2da803a280~mv2.png/v1/crop/x_45,y_0,w_544,h_488/fill/w_560,h_504,al_c,lg_1,q_85,enc_auto/2.png',
     },
     {
         id: 'm2',
         name: 'Schnitzel',
         description: 'A german specialty!',
         price: 16.5,
+        image: 'https://assets-eu-01.kc-usercontent.com/59e313da-7f86-0104-69e7-62c10f0c4ea4/dec3910a-ed6c-4b0d-afaa-b159f1403744/Wiener_Pouletschnitzel[1].png?auto=format&q=70&h=333&dpr=2.625',
     },
     {
         id: 'm3',
         name: 'Barbecue Burger',
         description: 'American, raw, meaty',
         price: 12.99,
+        image: burger,
     },
     {
         id: 'm4',
         name: 'Green Bowl',
         description: 'Healthy...and green...',
         price: 18.99,
+        image: 'https://www.greenbowl.com.do/wp-content/uploads/2021/03/Chicken-Honey-Mustard.png',
+    },
+    {
+        id: 'm5',
+        name: 'Cheese Pizza',
+        description: 'Cheese...and chicken...',
+        price: 28.99,
+        image: 'https://png.pngtree.com/png-clipart/20211009/original/pngtree-real-steak-cheese-top-view-food-pizza-png-image_6846673.png',
     },
 ];
