14cca9cb29d3d5ffbdbc5c2a6250bcc0ae29b98c
Asad Mumtaz <asadmumtaz92@gmail.com>
2023-07-07 05:35:31 +0500

    This commit for make custom function for formate currency...

---
+++ src/App/Helpers/CurrancyFormat.js
@@ -0,0 +1,7 @@
+// FOR AMOUNT FORMAT
+export const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+});
