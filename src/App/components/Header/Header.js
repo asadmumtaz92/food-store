618ecf5607737344e0fa0c37a131fb45b226147d
Asad Mumtaz <asadmumtaz92@gmail.com>
2023-07-08 18:16:32 +0500

    This commit for Add Checkout Button in header...

--- src/App/components/Header/Header.js
+++ src/App/components/Header/Header.js
@@ -1,20 +1,24 @@
 import React from "react"
 import styles from './styles/Header.module.css'
 import CartButton from '../Cart/CartButton'
+import Button from "../UI/Button"
 
 const Header = (props) => {
 
     return (
         <nav className={`navbar navbar-expand-sm bg-dark navbar-dark fixed-top ${styles.myNav}`}>
             <a className={`navbar-brand ${styles.logo}`} href="#">Food Store</a>
-            <ul className="navbar-nav" style={{ flex:1,'display': 'flex', 'justifyContent': 'end', alignItems: "end" }}>
+            <ul className="navbar-nav" style={{ flex: 1,'display': 'flex', 'justifyContent': 'end', alignItems: "end" }}>
+                {props?.cartLength > 0
+                    && <li className="nav-item">
+                        <Button title="Checkout" onClick={() => { props?.activeScreenHandler ('Checkout')}}/>
+                        {/* <a className="nav-link" href="#">Checkout</a> */}
+                    </li>
+                }
                 <li className="nav-item">
-                    {/* <a className="nav-link" href="#">Cart</a> */}
                     <CartButton cartLength={props?.cartLength} />
                 </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Login</a>
-                </li>
+
             </ul>
         </nav>
 
