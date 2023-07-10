5ad0e073759dd243a2fefb2f796e31d8c617c30b
Asad Mumtaz <asadmumtaz92@gmail.com>
2023-07-10 16:16:02 +0500

    This commit for Add LOGOUT Button in header...

--- src/App/components/Header/Header.js
+++ src/App/components/Header/Header.js
@@ -3,12 +3,25 @@ import styles from './styles/Header.module.css'
 import CartButton from '../Cart/CartButton'
 import Button from "../UI/Button"
 
+
 const Header = (props) => {
+
+    const logoutHandler = () => {
+        props?.OnConfirmOrder()
+        localStorage.setItem('Login', 'Login')
+        props?.activeScreenHandler('Login')
+    }
+
 
     return (
         <nav className={`navbar navbar-expand-sm bg-dark navbar-dark fixed-top ${styles.myNav}`}>
             <a className={`navbar-brand ${styles.logo}`} href="#">Food Store</a>
-            <ul className="navbar-nav" style={{ flex: 1,'display': 'flex', 'justifyContent': 'end', alignItems: "end" }}>
+            <ul className={`navbar-nav ${styles.myUl}`}>
+                {props?.activeScreenName !== 'Login'
+                    && <li className="nav-item">
+                        <Button title="Logout" onClick={logoutHandler} />
+                    </li>
+                }
                 {props?.activeScreenName == 'Home'
                     ? props?.cartLength > 0
                         && <li className="nav-item">
