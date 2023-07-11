f10995e0b95bbe6f9f57678bde8b28fdb7d480aa
Asad Mumtaz <asadmumtaz92@gmail.com>
2023-07-11 17:01:50 +0500

    This commit for comment data & that data upload in Firebase...

--- src/App/constantData/MealList.js
+++ src/App/constantData/MealList.js
@@ -1,52 +1,51 @@
-import burger from '../assets/images/foodItemIcon.png'
-export const DUMMY_MEALS = [
+// export const DUMMY_MEALS = [
     // {
     //     id: 'm0',
     //     name: 'Biryani',
     //     description: 'Rice, chicken, spicy...',
     //     price: 10.99,
-    //     image: 'https://thebiryaniwalla.com/images/biryani-1.png',
+    //     logo: 'https://thebiryaniwalla.com/images/biryani-1.png',
     // },
     // {
     //     id: 'm1',
     //     name: 'Sushi',
     //     description: 'Finest fish and veggies',
     //     price: 22.99,
-    //     image: 'https://static.wixstatic.com/media/d92b55_fb6f2e9db9f648a481576c2da803a280~mv2.png/v1/crop/x_45,y_0,w_544,h_488/fill/w_560,h_504,al_c,lg_1,q_85,enc_auto/2.png',
+    //     logo: 'https://static.wixstatic.com/media/d92b55_fb6f2e9db9f648a481576c2da803a280~mv2.png/v1/crop/x_45,y_0,w_544,h_488/fill/w_560,h_504,al_c,lg_1,q_85,enc_auto/2.png',
     // },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-        image: 'https://assets-eu-01.kc-usercontent.com/59e313da-7f86-0104-69e7-62c10f0c4ea4/dec3910a-ed6c-4b0d-afaa-b159f1403744/Wiener_Pouletschnitzel[1].png?auto=format&q=70&h=333&dpr=2.625',
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-        image: burger,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-        image: 'https://www.greenbowl.com.do/wp-content/uploads/2021/03/Chicken-Honey-Mustard.png',
-    },
-    {
-        id: 'm5',
-        name: 'Cheese Pizza',
-        description: 'Cheese...and chicken...',
-        price: 28.99,
-        image: 'https://png.pngtree.com/png-clipart/20211009/original/pngtree-real-steak-cheese-top-view-food-pizza-png-image_6846673.png',
-    },
-    {
-        id: 'm6',
-        name: 'BBQ Plater',
-        description: 'Chicken, meat, veggies',
-        price: 14.99,
-        image: 'https://images.squarespace-cdn.com/content/v1/57d11049f5e2315aa1308dd1/1490638332252-61DPSQOY01KS7JAUTDL0/Again+Re-Repeat+---recommended+---+Mixed+Platter.png?format=1000w',
-    },
-];
+    // {
+    //     id: 'm2',
+    //     name: 'Schnitzel',
+    //     description: 'A german specialty!',
+    //     price: 16.5,
+    //     logo: 'https://assets-eu-01.kc-usercontent.com/59e313da-7f86-0104-69e7-62c10f0c4ea4/dec3910a-ed6c-4b0d-afaa-b159f1403744/Wiener_Pouletschnitzel[1].png?auto=format&q=70&h=333&dpr=2.625',
+    // },
+    // {
+    //     id: 'm3',
+    //     name: 'Barbecue Burger',
+    //     description: 'American, raw, meaty',
+    //     price: 12.99,
+    //     logo: 'https://png.pngtree.com/png-clipart/20211009/original/pngtree-real-steak-cheese-top-view-food-pizza-png-image_6846673.png',
+    // },
+    // {
+    //     id: 'm4',
+    //     name: 'Green Bowl',
+    //     description: 'Healthy...and green...',
+    //     price: 18.99,
+    //     logo: 'https://www.greenbowl.com.do/wp-content/uploads/2021/03/Chicken-Honey-Mustard.png',
+    // },
+    // {
+    //     id: 'm5',
+    //     name: 'Cheese Pizza',
+    //     description: 'Cheese...and chicken...',
+    //     price: 28.99,
+    //     logo: 'https://png.pngtree.com/png-clipart/20211009/original/pngtree-real-steak-cheese-top-view-food-pizza-png-image_6846673.png',
+    // },
+    // {
+    //     id: 'm6',
+    //     name: 'BBQ Plater',
+    //     description: 'Chicken, meat, veggies',
+    //     price: 14.99,
+    //     logo: 'https://images.squarespace-cdn.com/content/v1/57d11049f5e2315aa1308dd1/1490638332252-61DPSQOY01KS7JAUTDL0/Again+Re-Repeat+---recommended+---+Mixed+Platter.png?format=1000w',
+    // },
+// ];
